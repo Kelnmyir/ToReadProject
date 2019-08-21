1464d41fda6998a100091a93725f5e8abac46692
Kelnmyir <50846948+Kelnmyir@users.noreply.github.com>
2019-08-21 21:33:43 +0300

    Created Book model and controller, some views

---
+++ ToRead/ToRead/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using ToRead.MVC.Models;
+
+namespace ToRead.MVC.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            var fakeBookList = new List<BookModel>(2) {
+                new BookModel() { Name="Pro C# 7", Annotation="You know why you need it"},
+                new BookModel() { Name="Репетитор по химии", Annotation="Больше не нужен"},
+            };
+            return View(fakeBookList);
+        }
+    }
+}
---
+++ ToRead/ToRead/Models/BookModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToRead.MVC.Models
+{
+    public class BookModel
+    {
+        public string Name { get; set; }
+
+        public string Annotation { get; set; }
+    }
+}
